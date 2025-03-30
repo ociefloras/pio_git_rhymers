34b8a03612bd4aaf157768cab11575f0bf927f5d
MateuszWrobel <251112@edu.p.lodz.pl>
2025-03-30 22:43:40 +0200

    8. Dodanie annotacji @Override

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,12 +2,14 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    private static final int DEFAULT_REJECTED_COUNT = 0;
+    private int totalRejected = DEFAULT_REJECTED_COUNT;
 
     public int reportRejected() {
         return totalRejected;
     }
 
+    @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
