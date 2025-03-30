8850415c04d9732b3f1671cd684cc9d1823ae43b
MateuszWrobel <251112@edu.p.lodz.pl>
2025-03-30 22:42:22 +0200

    6. Zamiana liczb za pomocą stałych wartości

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int CAPACITY = 12;
+    public static final int EMPTY = -1;
+    public static final int FULL = 11;
+    public static final int DEF = -1;
 
-    public int total = -1;
+    private int[] numbers = new int[CAPACITY];
+    private int total = EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,23 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return DEF;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return DEF;
         return numbers[total--];
     }
-
 }
