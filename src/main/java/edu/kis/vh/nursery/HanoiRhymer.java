8808a1db9882756cfdf8583a87a131d5b1a00049
Łukasz Balewicz <251121@edu.p.lodz.pl>
2025-04-01 13:41:04 +0200

    Rozwiązanie konfliktów merge z docs

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,15 +1,33 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa {@code HanoiRhymer} rozszerza {@code DefaultCountingOutRhymer} i implementuje
+ * wariant licznika, który odrzuca liczby według określonej zasady.
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * Licznik odrzuconych wartości
+     */
+    private int totalRejected = 0;
 
-    private static final int DEFAULT_REJECTED_COUNT = 0;
-    private int totalRejected = DEFAULT_REJECTED_COUNT;
-
+    /**
+     * Zwraca całkowitą liczbę odrzuconych wartości.
+     *
+     * @return liczba odrzuconych wartości
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * Nadpisuje metodę {@code countIn}, aby zaimplementować logikę odrzucania.
+     * Jeśli nowa wartość jest większa niż obecna wartość na szczycie stosu i stos nie jest pusty,
+     * wartość zostaje odrzucona, a licznik {@code totalRejected} zostaje zwiększony.
+     * W przeciwnym razie wartość jest dodawana do stosu.
+     *
+     * @param in wartość do dodania
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
